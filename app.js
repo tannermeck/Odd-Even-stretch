314854ed54ffce92b9143e86fa4e6e5964737f8d
TANNER MECK <steve.meck246@gmail.com>
2021-06-24 18:10:27 -0700

    bugs fixed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import { isEven } from './is-even.js';
 import { isOdd } from './is-even.js';
 // reference needed DOM elements
 const inputBox = document.getElementById('input-box');
-const button = document.getElementById('button);
+const button = document.getElementById('button');
 const result = document.getElementById('result');
 
 // set event listeners 
